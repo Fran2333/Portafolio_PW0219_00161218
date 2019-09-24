d86b251f19b4ac4d5779ad8c32cc3c473ecd868b
Fran2333 <00161218@uca.edu.sv>
2019-09-24 10:26:02 -0600

    ejercicio 7 terminado

---
+++ laboratorios/laboratorio 5/javascript/ejercicio7.js
@@ -0,0 +1,13 @@
+function fibunacci(numero){
+
+    let a=0,b=1,c,s=1;
+    console.log(a,b);
+    for(i = 3; i<=numero;i++){
+      c=a+b;
+      console.log(c);
+      s = s +c;
+      a =b;
+      b=c;
+    }  
+
+}
