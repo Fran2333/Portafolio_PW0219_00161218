6677e2853954e09f5523a1c0cbe7ff31a5cf5022
Fran2333 <00161218@uca.edu.sv>
2019-09-24 10:26:10 -0600

    ejercicio 8 terminado

---
+++ laboratorios/laboratorio 5/javascript/ejercicio8.js
@@ -0,0 +1,10 @@
+function conversor(numero){
+   
+    let respuesta="";
+    let numerodecimal= numero.toString(2);
+
+   respuesta=`El número decimal ${numero} en binario es ${numerodecimal}`;
+   console.log(respuesta);
+
+
+}
